5aa1fface403e0ac6eafe986ce8a5cf8fef2f651
Mandar <mandarkahare@gmail.com>
2022-12-21 15:26:09 +0530

    formatting

--- src/main/java/com/example/supplychainmanagement/HelloApplication.java
+++ src/main/java/com/example/supplychainmanagement/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.supplychainmanagement;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -15,6 +17,28 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    public static final int width=700, height =600, header=50;
+
+    Pane bodypane=new Pane();
+
+    private GridPane headerBar(){
+        TextField searchText=new TextField();
+        Button searchButton=new Button("Search");
+
+        GridPane gridpane=new GridPane();
+        gridpane.setMinSize(bodypane.getMinWidth(),header-10);
+        gridpane.setVgap(5);
+        gridpane.setHgap(5);
+//        gridpane.setStyle("-fx-background-color:BLUE");
+
+        gridpane.setAlignment(Pos.CENTER);
+
+        gridpane.add(searchText,0,0);
+        gridpane.add(searchButton,1,0);
+
+        return gridpane;
+    }
+
     private GridPane loginPage(){
         Label emailLabel=new Label("Email");
         Label passwordLabel =new Label("Password");
@@ -23,6 +47,13 @@ public class HelloApplication extends Application {
         PasswordField passwordfield=new PasswordField();
 
         GridPane gridpane=new GridPane();
+        gridpane.setMinSize(bodypane.getMinWidth(),bodypane.getMinHeight());
+        gridpane.setVgap(5);
+        gridpane.setHgap(5);
+        gridpane.setStyle("-fx-background-color:BLUE");
+
+//        gridpane.setAlignment(Pos.CENTER);
+
         gridpane.add(emailLabel,0,0);
         gridpane.add(emailTextField,1,0);
         gridpane.add(passwordLabel,0,1);
@@ -34,7 +65,13 @@ public class HelloApplication extends Application {
     private Pane createContent(){
         Pane root=new Pane();
 
-        root.getChildren().addAll(loginPage());
+        bodypane.setMinSize(width,height);
+
+        bodypane.getChildren().addAll(loginPage());
+        bodypane.setTranslateY(header);
+
+        root.setPrefSize(width,height+header);
+        root.getChildren().addAll(headerBar(),bodypane);
         return root;
     }
 
